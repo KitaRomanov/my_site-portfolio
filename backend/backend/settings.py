3ea457b22b007362241e120e53044ecc7679b6c5
Никита Ермишов <nikita321@bk.ru>
2023-06-25 22:05:38 +0300

    Проект переведен на постгрес

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -91,11 +91,14 @@ CACHES = {
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'ermishov_db',
+        'USER': 'ermishov_username',
+        'PASSWORD': 'password',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
